255243608c042187e8e105475a370b0227b5988f
Shahidnsu <shahid.khan@northsouth.edu>
2020-02-02 18:57:58 +0600

    learned about inject method

---
+++ software_engineerning_foundations/inject_method.rb
@@ -0,0 +1,17 @@
+# get sum of all the elements in the array
+
+ p [11,7,2,4].inject {|acc, el| acc + el}
+
+ #so how inject works it always maintain a varivable 
+ #that will change over the time ,that we will define in block
+
+ # First iteration
+ # acc = 11
+ #el = 7
+ #new_acc = 18
+
+
+ #second iteration:
+ #acc = 18
+ #el = 2
+ #new_acc = 20
